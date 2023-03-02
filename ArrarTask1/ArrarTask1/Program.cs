615eed32999e391a51f837518fd885b72f403dca
Faiqresulzade <faigbr@code.edu.az>
2023-03-03 00:06:07 +0400

    added 03.03.2023_Tasks

---
+++ ArrarTask1/ArrarTask1/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = { 3, 7, 8, 11, 19, 21 };
+int save_number = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+	for (int j =(numbers.Length-1)-i; j >0; j--)
+	{
+		if(!(i== j||numbers.Length==i))
+		{
+            save_number = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = save_number;
+            i++;
+            j--;
+        }
+		break;
+	}
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+	Console.WriteLine(numbers[i]);
+}
---
+++ ArrayTask2/ArrayTask2/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = { 1, 12, 23, 6, 9 };
+int count=0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if ((numbers[i] / 10) == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
